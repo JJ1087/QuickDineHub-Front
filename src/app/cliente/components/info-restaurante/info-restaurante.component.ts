8bc706bb366a361619ad085d31a36782d557e592
MiguelLher <milahe102@gmail.com>
2024-04-21 05:01:11 -0600

    perfil cliente, creacion de componentes para ver los restaurantes como cliente

---
+++ src/app/cliente/components/info-restaurante/info-restaurante.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-restaurante',
+  templateUrl: './info-restaurante.component.html',
+  styleUrl: './info-restaurante.component.css'
+})
+export class InfoRestauranteComponent {
+
+}
---
+++ src/app/cliente/components/restaurantes-cliente/restaurantes-cliente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurantes-cliente',
+  templateUrl: './restaurantes-cliente.component.html',
+  styleUrl: './restaurantes-cliente.component.css'
+})
+export class RestaurantesClienteComponent {
+
+}
